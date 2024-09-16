5b8576f3b3ad45e7bd3992aefa8e506beecd3413
LiuJie <1547698569@qq.com>
2024-09-17 00:05:38 +0800

    perf: environment variables for env file

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -1,7 +1,7 @@
-declare namespace NodeJS {
-  interface ProcessEnv {
+declare module 'bun' {
+  interface Env {
     MYSQL_DATABASE_URL: string
-    JWT_SECRET: string
+    JWT_SECRETS: string
     NODEMAILER_AUTH_EMAIL: string
     NODEMAILER_AUTH_PASS: string
   }
